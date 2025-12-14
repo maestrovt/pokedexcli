2ddeb217c04a0990f0f7bdcf574510a31d381605
maestrovt <maestrovt@gmail.com>
2025-12-14 12:58:40 -0500

    CH2_L1

--- go.mod
+++ go.mod
@@ -1,3 +1,2 @@
-module pokedexcli
-
+module github.com/bootdotdev/pokedexcli
 go 1.25.5
--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"time"
+
+	"github.com/bootdotdev/pokedexcli/internal/pokeapi"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	pokeClient := pokeapi.NewClient(5 * time.Second)
+	cfg := &config{
+		pokeapiClient: pokeClient,
+	}
+
+	startRepl(cfg)
 }
